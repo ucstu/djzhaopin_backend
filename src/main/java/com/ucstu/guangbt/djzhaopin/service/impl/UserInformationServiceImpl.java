baa55aad96e42b0554a352d36eae797ab749e785
张旭东 <2219454275@qq.com>
2022-04-20 11:31:27 +0000

    ssss

--- src/main/java/com/ucstu/guangbt/djzhaopin/service/impl/UserInformationServiceImpl.java
+++ src/main/java/com/ucstu/guangbt/djzhaopin/service/impl/UserInformationServiceImpl.java
@@ -47,45 +47,61 @@ public class UserInformationServiceImpl implements UserInformationService {
 
     @Override
     public JobExpectation createJobExpectation(UUID userinfoid, @Valid JobExpectation jobExpectation) {
-        // TODO Auto-generated method stub
-        return null;
+        UserInformation userInformation = userInformationRepository.findById(userinfoid).get();
+        userInformation.getJobExpectations().add(jobExpectation);
+        userInformation = userInformationRepository.save(userInformation);
+        return userInformation.getJobExpectations().get(userInformation.getJobExpectations().size() - 1);
     }
 
     @Override
     public JobExpectation deleteJobExpectationByJobExpectationId(UUID userinfoid, UUID jobexpectationid) {
-        // TODO Auto-generated method stub
-        return null;
+        UserInformation userInformation = userInformationRepository.findById(userinfoid).get();
+        JobExpectation jobExpectation = userInformation.getJobExpectations().stream()
+                .filter(j -> j.getJobExpectationId().equals(jobexpectationid)).findFirst().get();
+        userInformation.getJobExpectations().remove(jobExpectation);
+        return jobExpectation;
     }
 
     @Override
     public JobExpectation updateJobExpectationByJobExpectationId(UUID userinfoid, UUID jobexpectationid,
             JobExpectation jobExpectation) {
-        // TODO Auto-generated method stub
-        return null;
+        UserInformation userInformation = userInformationRepository.findById(userinfoid).get();
+        JobExpectation jobExpectation1 = userInformation.getJobExpectations().stream()
+                .filter(j -> j.getJobExpectationId().equals(jobexpectationid)).findFirst().get();
+        userInformation.getJobExpectations().remove(jobExpectation1);
+        userInformation.getJobExpectations().add(jobExpectation);
+        userInformation = userInformationRepository.save(userInformation);
+        return userInformation.getJobExpectations().get(userInformation.getJobExpectations().size() - 1);
     }
 
     @Override
     public List<JobExpectation> getJobExpectations(UUID userinfoid) {
-        // TODO Auto-generated method stub
-        return null;
+        UserInformation userInformation = userInformationRepository.findById(userinfoid).get();
+        return userInformation.getJobExpectations();
     }
 
     @Override
     public JobExpectation getJobExpectationByJobExpectationId(UUID userinfoid, UUID jobexpectationid) {
-        // TODO Auto-generated method stub
-        return null;
+        UserInformation userInformation = userInformationRepository.findById(userinfoid).get();
+        return userInformation.getJobExpectations().stream()
+                .filter(j -> j.getJobExpectationId().equals(jobexpectationid)).findFirst().get();
     }
 
     @Override
     public EducationExperience createEducationExperience(UUID userinfoid, EducationExperience educationExperience) {
-        // TODO Auto-generated method stub
-        return null;
+        UserInformation userInformation = userInformationRepository.findById(userinfoid).get();
+        userInformation.getEducationExperiences().add(educationExperience);
+        userInformation = userInformationRepository.save(userInformation);
+        return userInformation.getEducationExperiences().get(userInformation.getEducationExperiences().size() - 1);
     }
 
     @Override
     public EducationExperience deleteEducationExperienceByEducationExperienceId(UUID userinfoid, UUID eduexperienceid) {
-        // TODO Auto-generated method stub
-        return null;
+        UserInformation userInformation = userInformationRepository.findById(userinfoid).get();
+        EducationExperience educationExperience = userInformation.getEducationExperiences().stream()
+                .filter(e -> e.getEducationExperienceId().equals(eduexperienceid)).findFirst().get();
+        userInformation.getEducationExperiences().remove(educationExperience);
+        return educationExperience;
     }
 
     @Override
