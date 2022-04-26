3fb14cafe65941c05c328dfe67aea677fdb755cc
张旭东 <2219454275@qq.com>
2022-04-26 15:11:45 +0000

    123

--- src/main/java/com/ucstu/guangbt/djzhaopin/model/account/ForgetPasswordRequest.java
+++ src/main/java/com/ucstu/guangbt/djzhaopin/model/account/ForgetPasswordRequest.java
@@ -12,7 +12,7 @@ import lombok.experimental.Accessors;
 @AllArgsConstructor
 @Accessors(chain = true)
 public class ForgetPasswordRequest {
-    @Pattern(regexp = "^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\\d{8}$")
+    @Pattern(regexp = "^1[3456789]\\d{9}$")
     private String userName;
 
     @Size(min = 6, max = 20)
--- src/main/java/com/ucstu/guangbt/djzhaopin/model/account/LoginAccountRequest.java
+++ src/main/java/com/ucstu/guangbt/djzhaopin/model/account/LoginAccountRequest.java
@@ -14,7 +14,7 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class LoginAccountRequest {
     @NotBlank
-    @Pattern(regexp = "^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\\d{8}$")
+    @Pattern(regexp = "^1[3456789]\\d{9}$")
     private String userName;
     @NotBlank
     @Size(min = 6, max = 20)
