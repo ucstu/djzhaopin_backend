8875fa917b2060963674923d8bc195485fb7c98d
张旭东 <2219454275@qq.com>
2022-04-26 14:33:31 +0000

    12341234

--- src/main/java/com/ucstu/guangbt/djzhaopin/controller/company/CompanyInformationCotroller.java
+++ src/main/java/com/ucstu/guangbt/djzhaopin/controller/company/CompanyInformationCotroller.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 
 @Validated
+@CrossOrigin
 @RestController
 @RequestMapping("/companyinfos")
 public class CompanyInformationCotroller {
