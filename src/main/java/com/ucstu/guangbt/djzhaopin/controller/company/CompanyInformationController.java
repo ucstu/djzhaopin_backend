9efcbe81c4f101d203ad9c17cf59954392028f3f
张旭东 <2219454275@qq.com>
2022-04-30 13:55:08 +0000

    修复了一个错误

--- src/main/java/com/ucstu/guangbt/djzhaopin/controller/company/CompanyInformationController.java
+++ src/main/java/com/ucstu/guangbt/djzhaopin/controller/company/CompanyInformationController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,7 +45,7 @@ public class CompanyInformationController {
                                 .createCompanyInformation(companyInformation));
         }
 
-        @PostMapping("{companyInfoId}")
+        @PutMapping("{companyInfoId}")
         public ResponseEntity<ResponseBody<CompanyInformation>> updateCompanyInformationByCompanyInformationId(
                         @PathVariable("companyInfoId") @NotNull UUID companyInformationId,
                         @Valid @RequestBody CompanyInformation companyInformation) {
