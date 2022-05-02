3975bfa26e1917d282e187f3b47143f3c0848981
张旭东 <2219454275@qq.com>
2022-05-02 05:55:38 +0000

    修复错误

--- src/main/java/com/ucstu/guangbt/djzhaopin/entity/user/UserInformation.java
+++ src/main/java/com/ucstu/guangbt/djzhaopin/entity/user/UserInformation.java
@@ -125,6 +125,9 @@ public class UserInformation {
 
     @JsonGetter("age")
     public Integer getAge() {
+        if (dateOfBirth == null) {
+            return 0;
+        }
         return (int) ((new Date().getTime() - dateOfBirth.getTime()) / (1000 * 60 * 60 * 24) / 365);
     }
 
