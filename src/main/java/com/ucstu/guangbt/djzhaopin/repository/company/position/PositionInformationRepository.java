a6afc03e75fe4bc6997be3fba86859a0e18112df
张旭东 <2219454275@qq.com>
2022-05-06 06:42:36 +0000

    修复问题

--- src/main/java/com/ucstu/guangbt/djzhaopin/repository/company/position/PositionInformationRepository.java
+++ src/main/java/com/ucstu/guangbt/djzhaopin/repository/company/position/PositionInformationRepository.java
@@ -5,9 +5,8 @@ import java.util.UUID;
 import com.ucstu.guangbt.djzhaopin.entity.company.position.PositionInformation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface PositionInformationRepository
-        extends JpaRepository<PositionInformation, UUID>, QuerydslPredicateExecutor<PositionInformation> {
+                extends JpaRepository<PositionInformation, UUID> {
 
 }
