4a771586a2db283a8f16ddf6e0e89193f73a430f
张旭东 <2219454275@qq.com>
2022-05-06 06:28:46 +0000

    我爱死你焊工呢鞥

--- src/main/java/com/ucstu/guangbt/djzhaopin/repository/company/position/PositionInformationRepository.java
+++ src/main/java/com/ucstu/guangbt/djzhaopin/repository/company/position/PositionInformationRepository.java
@@ -5,7 +5,9 @@ import java.util.UUID;
 import com.ucstu.guangbt.djzhaopin.entity.company.position.PositionInformation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface PositionInformationRepository extends JpaRepository<PositionInformation, UUID> {
+public interface PositionInformationRepository
+        extends JpaRepository<PositionInformation, UUID>, QuerydslPredicateExecutor<PositionInformation> {
 
 }
