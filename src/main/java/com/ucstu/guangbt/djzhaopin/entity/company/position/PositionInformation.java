f83aebb33fe9cd4f53f11883d40155889b379f92
张旭东 <2219454275@qq.com>
2022-05-08 07:02:58 +0000

    修复问题

--- src/main/java/com/ucstu/guangbt/djzhaopin/entity/company/position/PositionInformation.java
+++ src/main/java/com/ucstu/guangbt/djzhaopin/entity/company/position/PositionInformation.java
@@ -35,6 +35,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
