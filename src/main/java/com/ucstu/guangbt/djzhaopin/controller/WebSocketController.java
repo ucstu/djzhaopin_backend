66b125bae33135d32d862486720323b4f3c37b46
张旭东 <2219454275@qq.com>
2022-05-10 10:17:14 +0000

    添加心跳

--- src/main/java/com/ucstu/guangbt/djzhaopin/controller/WebSocketController.java
+++ src/main/java/com/ucstu/guangbt/djzhaopin/controller/WebSocketController.java
@@ -6,6 +6,9 @@ import com.ucstu.guangbt.djzhaopin.entity.util.MessageRecord;
 import com.ucstu.guangbt.djzhaopin.service.MessageRecordService;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 import jakarta.annotation.Resource;
@@ -16,9 +19,24 @@ public class WebSocketController {
     @Resource
     private MessageRecordService messageRecordService;
 
+    @Resource
+    private SimpMessagingTemplate simpMessagingTemplate;
+
     @MessageMapping("/message")
     public void sendUserMessage(Principal principal, MessageRecord messageRecord) {
         messageRecordService.sendUserMessage(principal, messageRecord);
+    }
+
+    @Scheduled(fixedDelay = 20000L)
+    @SendTo("/topic/pingpong")
+    public void sendPong() {
+        simpMessagingTemplate.convertAndSend("/topic/pingpong", "pong (periodic)");
+    }
+
+    @MessageMapping("/ping")
+    @SendTo("/topic/pingpong")
+    public String sendPingResponse() {
+        return "pong (response)";
     }
 
 }
